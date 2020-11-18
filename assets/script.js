b561f35fc2f839ed98072d2f62c46eabe9e2c5ff
Johnnie Simpson <johnniesimpson@outlook.com>
2020-11-17 23:43:10 -0500

    Updated links to JS and CSS in HTML file. Beginning to add variables to JS file.

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,15 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+    
+}
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+var lettersLower = ["abcdefghijklmnopqrstuvwxyz".split("")];
+
+var lettersUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")]
+
+console.log(lettersLower)
+console.log(lettersUpper)
