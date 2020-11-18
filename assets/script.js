0d5bafc366175699f75926f0602ed3c72aec5ade
Johnnie Simpson <johnniesimpson@outlook.com>
2020-11-18 00:24:37 -0500

    Added variables for numbers, lowercase letters, uppercase letters, and unicode special characters.

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,18 @@ generateBtn.addEventListener("click", writePassword); {
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-var lettersLower = ["abcdefghijklmnopqrstuvwxyz".split("")];
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+"t", "u", "v", "w", "x", "y", "z"];
 
-var lettersUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")]
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+"T", "U", "V", "W", "X", "Y", "Z"];
 
-console.log(lettersLower)
-console.log(lettersUpper)
+var specialChar = ['\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u0027', '\u0028', '\u0029',
+'\u002a', '\u002b', '\u002c', '\u002d', '\u002e', '\u002f', '\u003a', '\u003b', '\u003c', '\u003d', '\u003e',
+'\u003f', '\u0040', '\u005b', '\u005c', '\u005d', '\u005e', '\u005f', '\u0060', '\u007b', '\u007c', '\u007d',
+'\u007e']
+
+console.log(lettersLower);
+console.log(lettersUpper);
+console.log(numbers);
+console.log(specialChar);
