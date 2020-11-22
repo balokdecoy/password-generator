04d6332fbf1880c3fce534a7d8bab538cda83bd2
Johnnie Simpson <johnniesimpson@outlook.com>
2020-11-22 09:14:21 -0500

    Added code to reset the password concatenation array after the program runs so the user can generate a new password with a fresh array without having to refresh the page.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 
+// Password character arrays
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
@@ -13,14 +14,17 @@ var unicode = ['\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u00
 '\u003f', '\u0040', '\u005b', '\u005c', '\u005d', '\u005e', '\u005f', '\u0060', '\u007b', '\u007c', '\u007d',
 '\u007e'];
 
+// Password array to be concatenated based on user inputs
 var passwordArr = [];
 
+// Console log character arrays as check
 console.log(lettersLower);
 console.log(lettersUpper);
 console.log(numbers);
 console.log(unicode);
 console.log(passwordArr);
 
+//Pull generate button tag from html file
 var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
@@ -30,14 +34,18 @@ function writePassword() {
   
   passwordText.value = password;
 
+  //Password generation function. 
   function generatePassword() {
     alert("Welcome to the Password Generator!");
+    // Asks user for their desired password length. Used later in code as iteration number for password array loop
     var charNumber = prompt("Enter a password character length between 8 and 128.");
     charNumber = parseInt(charNumber);
     console.log(charNumber);
 
+    // If else function ensuring user input is valid. 
     if (typeof charNumber == "number" && charNumber <= 128 && charNumber >= 8) {
       var confNumbers = confirm("Do you want numbers in your password?");
+      //Concatenate the numbers array to the password array if user selects true
       if (confNumbers === true) {
         passwordArr = passwordArr.concat(numbers);
         console.log(passwordArr);
@@ -46,6 +54,7 @@ function writePassword() {
         confNumbers === false;
       }
 
+      //Concatenate unicode array to the password array if user selects true
       var confSpecial = confirm("Do you want special characters in your password?");
       if (confSpecial === true) {
         passwordArr = passwordArr.concat(unicode);
@@ -55,6 +64,7 @@ function writePassword() {
         confSpecial === false;
       }
 
+      //Concatenate uppercase letters array to the password array if user selects true
       var confUpper = confirm("Do you want uppercase letters in your password?");
       if (confUpper === true) {
         passwordArr = passwordArr.concat(lettersUpper);
@@ -64,6 +74,7 @@ function writePassword() {
         confUpper === false;
       }
 
+      //Concatenate lowercase letters array to the password array if user selects true
       var confLower = confirm("Do you want lowercase letters in your password?");
       if (confLower === true) {
         passwordArr = passwordArr.concat(lettersLower);
@@ -73,19 +84,25 @@ function writePassword() {
         confLower === false;
       }
 
+      // Console log the fully concatenated password array according to user inputs
       console.log(passwordArr);
 
+      // Validation to ensure user selects true for at least one password element
       if (confNumbers === false && confSpecial === false && confUpper === false && confLower === false) {
         alert("Please choose at least 1 password element.");
         passwordText = "Please choose at least 1 password element.";
         return passwordText;
       }
+
+      // If user input is valid, run loop grabbing a character from the password array at random and write result to the password text box
       else {
         var passwordText = "";
         for (var i = 0; i < charNumber; i++) {
           var charSelect = Math.floor(Math.random() * passwordArr.length);
           passwordText += passwordArr[charSelect];
         }
+        
+        // Return password text to the HTML document
         console.log(passwordText);
         return passwordText;
       }
@@ -94,6 +111,8 @@ function writePassword() {
       alert("Please choose a number between 8 and 128.");
     }
   }
+  // Reset the password array so user can rerun the program with a fresh array without having to refresh the page
+  passwordArr = [];
 }
 
 // Add event listener to generate button
